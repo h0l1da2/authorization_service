182efd07f1d16cde1fb1fde92469832429c5c2fa
h0l1da2 <holiday.k1@icloud.com>
2024-12-17 23:55:06 +0900

    remove(sub): 예외 삭제 (서브모듈로 해결)
    holiday

--- src/main/java/me/holiday/exception/ServerException.java
+++
@@ -1,9 +0,0 @@
-package me.holiday.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class ServerException extends DefaultException {
-    public ServerException() {
-        super(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER ERROR", null);
-    }
-}
